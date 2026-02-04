7ab17afc77cf1340ad036503dcc03fe4dff3e97b
Angy Zamorano <zangehoshi@gmail.com>
2026-02-03 23:01:44 -0600

    pls im begging

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // Importamos la imagen dándole un nombre (puedes llamarle gaturrona)
-import gaturrona from '../public/Sources_Images/gaturrona.jpg';
+import gaturrona from './Sources_Images/gaturrona.jpg';
 
 function App() {
   return (
